3dc6ae4fc4944012711eceb6a25a47c833822141
anitayyc <yy3557@nyu.edu>
2023-09-28 03:56:40 -0400

    update fetch AboutUs from backend

--- front-end/src/AboutUs.js
+++ front-end/src/AboutUs.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
 import './AboutUs.css'
 
 /**
@@ -6,20 +7,35 @@ import './AboutUs.css'
  * @param {*} param0 an object holding any props passed to this component from its parent component
  * @returns The contents of this component, in JSX form.
  */
-const AboutUs = props => {
+const AboutUs = () => {
+  const [aboutUsData, setAboutUsData] = useState({
+    about: '',
+    pictureUrl: '',
+  });
+
+
+  useEffect(() => {
+    fetch('http://localhost:5002/about')
+      .then(response => response.json())
+      .then(data => {
+        console.log('About Us Data:', data);  // Log the data
+        setAboutUsData(data);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+  
+
+
+  //const pictureUrl = 'http://localhost:5002/AboutUsPicture.jpg';
+
   return (
     <div>
       <h2>About Us</h2>
-      <p>Hello! My name is Anita Ye and I am a senior majoring in math/cs.</p>
-      {/* Add your photo here */}
-      <img src="/AboutUsPicture.jpeg" alt="AboutUsPicture"
-      style = {{
-        width: '300px',
-        height: 'auto'
-      }} />
+      <p>{aboutUsData.about}</p>
+      <img src={aboutUsData.pictureUrl} alt="About Us Picture" style={{ width: '300px', height: 'auto' }} />
     </div>
-  )
-}
+  );
+};
 
 // make this component available to be imported into any other file
 export default AboutUs
